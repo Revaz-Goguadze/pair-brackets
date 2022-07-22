9a153e57f2b65347fe6b0441aec43e115cf8486b
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-07-22 14:29:03 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ PairBrackets/StringExtensions.cs
@@ -0,0 +1,51 @@
+﻿namespace PairBrackets
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Returns the number of bracket pairs in the <see cref="text"/>.
+        /// </summary>
+        /// <param name="text">The source text.</param>
+        /// <returns>The number of bracket pairs in the <see cref="text"/>.</returns>
+        public static int CountBracketPairs(this string text)
+        {
+            // TODO #1. Analyze the method unit tests and add the method implementation.
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Searches the <see cref="text"/> and returns the list of start and end positions of bracket pairs.
+        /// </summary>
+        /// <param name="text">The source text.</param>
+        /// <returns>The list of start and end positions of bracket pairs.</returns>
+        /// <exception cref="ArgumentNullException"><see cref="text"/> is null.</exception>
+        public static IList<(int, int)> GetBracketPairPositions(this string? text)
+        {
+            // TODO #2. Analyze the method unit tests and add the method implementation.
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Examines the <see cref="text"/> and returns true if the pairs and the orders of brackets are balanced; otherwise returns false.
+        /// </summary>
+        /// <param name="text">The source text.</param>
+        /// <param name="bracketTypes">The bracket type option.</param>
+        /// <returns>True if the pairs and the orders of brackets are balanced; otherwise returns false.</returns>
+        /// <exception cref="ArgumentNullException"><see cref="text"/> is null.</exception>
+        public static bool ValidateBrackets(this string? text, BracketTypes bracketTypes)
+        {
+            // TODO #3. Analyze the method unit tests and add the method implementation.
+            throw new NotImplementedException();
+        }
+    }
+
+    [Flags]
+    public enum BracketTypes
+    {
+        All = 0,
+        RoundBrackets = 1,
+        SquareBrackets = 2,
+        CurlyBrackets = 3,
+        AngleBrackets = 4,
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# Pair Brackets
+
+An intermediate level task for practicing algorithms, collections and enumeration types.
+
+Estimated time to complete the task - 1h.
+
+The task requires .NET 6 SDK installed.
+
+
+## Task Description
+
+In this task you have to implement three [extension methods](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods) that process a text and searches for pair of brackets.
+
+Use (.NET collections)[https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/collections] to solve this task. Implement a method using a different collection, so each method should use a collection that is not used in other methods.
+
+
+### 1. Bracket Pairs Number
+
+Implement the [CountBracketPairs](PairBrackets/StringExtensions.cs#L10) method to return the number of bracket pairs in the `text`.
+
+The method should support these types of brackets:
+* Round brackets ()
+* Square brackets []
+
+
+### 2. Bracket Pair Positions
+
+Implement the [GetBracketPairPositions](PairBrackets/StringExtensions.cs#L22) method to return the list of indexes of an open and closed positions of bracket pairs.
+
+The method should support these types of brackets:
+* Round brackets ()
+* Square brackets []
+* Curly brackets {}
+
+
+### 3. Bracket Validator
+
+Implement the [ValidateBrackets](PairBrackets/StringExtensions.cs#L35) method to examine the `text` and returns true if the pairs and the orders of brackets are balanced. Otherwise the method should return false.
+
+The method has the `bracketTypes` parameter that specifies what kind of brackets should be validated. The parameter is of the [enumeration type](https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/enum).
